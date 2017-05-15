0eeca3dcb6808d9e235fc0c2d84e40cefa4e747a
Ivan Antunović <doyouevenavr@gmail.com>
2017-05-15 19:39:44 +0200

    Add files via upload

---
+++ src/BluetoothTransmitter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BluetoothController
+{
+    class BluetoothTransmitter
+    {
+        Stream clientStream;
+
+        public BluetoothTransmitter(Stream clientStream)
+        {
+            this.clientStream = clientStream;
+        }
+
+        public bool canWrite()
+        {
+            return this.clientStream.CanWrite;
+        }
+
+        public void writeBytes(byte[] buffer, int offset, int count)
+        {
+            try
+            {
+                this.clientStream.Write(buffer, offset, count);
+                this.clientStream.Flush();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex);
+                Console.WriteLine("Client has disconnected!!!!");
+            }
+        }
+
+        public void writeString(string nString)
+        {
+            byte[] txBuffer = new byte[1024];
+            txBuffer = Encoding.ASCII.GetBytes(nString);
+
+            try
+            {
+                this.clientStream.Write(txBuffer, 0, txBuffer.Length);
+                this.clientStream.Flush();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex);
+                Console.WriteLine("Client has disconnected!!!!");
+            }
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BluetoothController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BluetoothController bluetooth = new BluetoothController();
+            string keyboardInput;
+            int paireddeviceInfo = -1;
+            string deviceInfoSelected ="";
+
+            Console.WriteLine("Enter 'bt -commands' for all available commands.");
+            Console.WriteLine("In order to connect to a specific deviceInfo, " +
+                                "deviceInfos need to be paired first!");
+
+            while (true)
+            {
+                Console.Write(">> ");
+                keyboardInput = Console.ReadLine();
+                if (keyboardInput.Equals("bt -device -discover"))
+                {
+                    bluetooth.discoverDevices();
+                }
+                else if (keyboardInput.StartsWith("bt -device -select"))
+                {
+                    keyboardInput = keyboardInput.TrimStart("bt -deviceInfo -select".ToCharArray());
+                    if (!bluetooth.isDeviceFound(keyboardInput))
+                    {
+                        Console.WriteLine("<< Invalid device selected.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("<< deviceInfo {0} slected.", keyboardInput);
+                        deviceInfoSelected = keyboardInput;
+                    }
+                }
+                else if (keyboardInput.Equals("bt -connect"))
+                {
+                    bluetooth.connect();
+                }
+                else if (keyboardInput.Equals("bt -pair"))
+                {
+                    paireddeviceInfo = bluetooth.pairDevice(deviceInfoSelected);
+                }
+                else if (keyboardInput.StartsWith("bt -transmit "))
+                {
+                    keyboardInput = keyboardInput.TrimStart("bt -input ".ToCharArray());
+                    if (keyboardInput.Length > 0)
+                    {
+                        bluetooth.writeString(keyboardInput);
+                        Console.WriteLine("Text has been sent.");
+                    }
+                    else
+                    { 
+                        Console.WriteLine("No text to be sent!");
+                    }
+                }
+                else if (keyboardInput.Equals("bt -commands"))
+                {
+                    Console.WriteLine("<< {0, -3}", "Available commands:" );
+                    Console.WriteLine("bt -device -discover");
+                    Console.WriteLine("bt -device -select [deviceInfo Name]");
+                    Console.WriteLine("bt -pair");
+                    Console.WriteLine("bt -connect");
+                    Console.WriteLine("bt -transmit [Text]");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Command");
+                } 
+            }
+        }
+
+ 
+    }
+}
